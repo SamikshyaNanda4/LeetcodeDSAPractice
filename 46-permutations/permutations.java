582127343e61b19cfdb722740c70f178a1d3fe9f
Sam Nanda <91324968+SamikshyaNanda4@users.noreply.github.com>
2026-04-22 15:35:37 +0530

    Time: 1 ms (85.90%) | Memory: 45.6 MB (32.62%) - LeetSync

---
+++ 46-permutations/permutations.java
@@ -0,0 +1,30 @@
+class Solution {
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> ansList=new ArrayList<>();
+
+        backtrack(ansList, new ArrayList<Integer>(), nums);
+        return ansList;
+    }
+
+    public static void backtrack(List<List<Integer>> resList,ArrayList<Integer> tempList, int[] nums){
+
+        //base condition 
+        if(tempList.size()==nums.length){
+            resList.add(new ArrayList<>(tempList));
+            return;
+        }
+
+        //loops through permutes
+        for(int number:nums){
+            //if already present skip as we need everytime latest
+            if(tempList.contains(number)) 
+            continue;
+            //add the number latest not present 
+            tempList.add(number);
+
+            backtrack(resList, tempList, nums);
+
+            tempList.remove(tempList.size()-1);
+        }
+    }
+}
